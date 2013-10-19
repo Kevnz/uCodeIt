64ed65ae67d03316729f006ed101dd72e4b8d2fc
benjamin.howarth <benjamin@codegecko.com>
2013-10-20 00:28:39 +0100

    Default strategy added with factory for merging doctypes. Added new repository with a view to improving performances of doctype retrieval.

---
+++ uCodeIt/BespokePetaPocoRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uCodeIt
+{
+    class BespokePetaPocoRepository
+    {
+        public void DoSomething()
+        {
+
+
+        }
+    }
+}
---
+++ uCodeIt/DocumentTypes/TabGroupAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uCodeIt.DocumentTypes
+{
+    public class TabGroupAttribute : Attribute
+    {
+        public TabGroupAttribute(string name)
+        {
+            TabName = name;
+        }
+
+        public string TabName { get; set; }
+    }
+}
