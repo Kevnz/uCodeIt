885b542332037f7a7a95b05c11192d792b5064c2
Aaron Powell <me@aaron-powell.com>
2013-10-23 10:38:39 +1100

    Making RenderModel the base class

--- uCodeIt.Demo/Models/DocumentTypes/HomePage.cs
+++ uCodeIt.Demo/Models/DocumentTypes/HomePage.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using uCodeIt.DocumentTypes;
+using Umbraco.Core.Models;
 
 namespace uCodeIt.Demo.Models.DocumentTypes
 {
     [DocumentType(Alias = "Home", AllowedChildren = new[] { typeof(HomePage) }, Templates = new[] { "Home Page", "Alt Home Page" })]
     public class HomePage : DocumentTypeBase
     {
+        public HomePage(IPublishedContent content) 
+            : base(content)
+        {
+        }
+
         [Property(DataType = DataType.TextString, Tab = Tab.Tab2, Name = "Second Title")]
         public string Title { get; set; }
 
--- uCodeIt/DocumentTypes/DocumentTypeBase.cs
+++ uCodeIt/DocumentTypes/DocumentTypeBase.cs
@@ -1,8 +1,19 @@
-﻿namespace uCodeIt.DocumentTypes
+﻿using System.Globalization;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+using Umbraco.Web.Models;
+
+namespace uCodeIt.DocumentTypes
 {
-    public abstract class DocumentTypeBase
+    public abstract class DocumentTypeBase : RenderModel
     {
-        protected DocumentTypeBase()
+        protected DocumentTypeBase(IPublishedContent content)
+            : this(content, UmbracoContext.Current.PublishedContentRequest.Culture)
+        {
+        }
+
+        protected DocumentTypeBase(IPublishedContent content, CultureInfo cultureInfo)
+            : base(content, cultureInfo)
         {
         }
     }
