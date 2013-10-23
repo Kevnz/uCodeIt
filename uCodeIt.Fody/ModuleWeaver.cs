8e4c107127b50f20e34bc184e38861ff008d0b59
Aaron Powell <me@aaron-powell.com>
2013-10-23 11:18:27 +1100

    Getting started with fody plugin

---
+++ uCodeIt.Fody/ModuleWeaver.cs
@@ -0,0 +1,16 @@
+﻿using Mono.Cecil;
+using System.Linq;
+using uCodeIt.DocumentTypes;
+
+namespace uCodeIt.Fody
+{
+    public class ModuleWeaver
+    {
+        public ModuleDefinition ModuleDefinition { get; set; }
+
+        public void Execute()
+        {
+            //var types = ModuleDefinition.Types.Where(type => type.CustomAttributes.Any(attr => attr.AttributeType.FullName == typeof(DocumentTypeAttribute).FullName));
+        }
+    }
+}
